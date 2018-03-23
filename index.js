4f16e8255786e01dcb3a848d4ff5e6145bf67d4f
Matus Novak <matus.novak@hindsightsoftware.co.uk>
2018-03-23 12:44:43 +0000

    Added initial dialects to convert between gherkin and mongo languages

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var validators = require('./helpers/validators')
 var RequestError = require('./helpers/request-error')
 var WebHooks = require('./helpers/webhooks')
 var filters = require('./helpers/filters')
+var dialects = require('./helpers/dialects')
 
 module.exports.http = http
 module.exports.validators = validators
@@ -11,3 +12,4 @@ module.exports.routes = routes
 module.exports.RequestError = RequestError
 module.exports.WebHooks = WebHooks
 module.exports.pick = filters.pick
+module.exports.GHERKIN_LANG_TO_MONGO_LANG = dialects.GHERKIN_LANG_TO_MONGO_LANG
