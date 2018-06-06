2303d58645ff5bc328a8bda3ae883b5b8a4d8dcb
Matus Novak <matus.novak@hindsightsoftware.co.uk>
2018-06-06 10:21:49 +0100

    request wrapper fix response is null

--- helpers/request-wrapper.js
+++ helpers/request-wrapper.js
@@ -1,7 +1,7 @@
 
 module.exports = function (req, res, next, func) {
   func(req).then(result => {
-    if (result !== undefined) res.status(200).send(result)
+    if (result !== undefined && result !== null) res.status(200).send(result)
     else res.status(204).send()
   }).catch(err => {
     next(err)
