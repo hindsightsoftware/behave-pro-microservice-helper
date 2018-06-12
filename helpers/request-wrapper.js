5908675758152b94a5b7a6525f5fb26758f83c4c
Matus Novak <matus.novak@hindsightsoftware.co.uk>
2018-06-12 12:05:31 +0100

    Wrap requestWrapper in try catch

--- helpers/request-wrapper.js
+++ helpers/request-wrapper.js
@@ -1,9 +1,13 @@
 
 module.exports = function (req, res, next, func) {
-  func(req).then(result => {
-    if (result !== undefined && result !== null) res.status(200).send(result)
-    else res.status(204).send()
-  }).catch(err => {
+  try {
+    func(req).then(result => {
+      if (result !== undefined && result !== null) res.status(200).send(result)
+      else res.status(204).send()
+    }).catch(err => {
+      next(err)
+    })
+  } catch (err) {
     next(err)
-  })
+  }
 }
