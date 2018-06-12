e6440c0c2ceb2a9adad4dda3e917f72f8cae32b8
Matus Novak <matus.novak@hindsightsoftware.co.uk>
2018-06-12 15:52:01 +0100

    Use empty string if tenantKey or traceId is missing

--- helpers/default-response-handler.js
+++ helpers/default-response-handler.js
@@ -1,12 +1,12 @@
 const LoggerV2 = require('./logger-v2')
 
 function logResponse (req, res) {
-  var tenantKey = req.query.tenantKey || 'undefined'
-  const traceId = req.traceId || 'undefined'
-  if (req.swagger && req.swagger.params.tenantKey) tenantKey = req.swagger.params.tenantKey.value || 'undefined'
+  var tenantKey = req.query.tenantKey || ''
+  const traceId = req.traceId || ''
+  if (req.swagger && req.swagger.params.tenantKey) tenantKey = req.swagger.params.tenantKey.value || ''
   const level = res.statusCode >= 400 ? 'error' : 'info'
 
-  LoggerV2[level](tenantKey, traceId, req.method, req._parsedUrl.pathname, res.statusCode, '(', req.traceId, ')')
+  LoggerV2[level](tenantKey, traceId, req.method, req._parsedUrl.pathname, res.statusCode)
 }
 
 module.exports = function (req, res, next) {
